c8bbc3c82725c0c173f32262a4ee9ab20d701f08
leadjockey <dltldnjs5790@naver.com>
2018-04-04 20:58:02 +0900

    [update] 라우트와 파라미터 이해하기

--- test/src/shared/App.js
+++ test/src/shared/App.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import {Route,Switch}            from 'react-router-dom';
+import {Home, About}      from 'pages';
+
 
 class App extends Component{
 	render(){
 		return (
-			<div>Hello React-Router</div>
-		)
+			<div>
+				<Route exact path="/" component={Home}/>
+				<Switch>
+					<Route path="/about/:name" component={About}/>
+					<Route path="/about" component={About}/>
+				</Switch>
+			</div>
+		);
 	}
 }
 
